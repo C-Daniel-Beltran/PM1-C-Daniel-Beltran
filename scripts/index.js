f18ff46e9bc36fc200bbec7ffe352aa4c9fd70e1
danielbel91 <danielbel91@hotmail.com>
2024-09-11 11:19:46 -0300

    test is OK

--- scripts/index.js
+++ scripts/index.js
@@ -9,16 +9,87 @@ class Activity {
 
 class Repository {
   constructor() {
+    this.id = 0;
     this.activities = [];
   }
   getAllActivities() {
-    this.activities.forEach((element) => console.log(element));
+    return this.activities;
   }
-  createActivity(id, title, description, imgUrl) {
+  createActivity(title, description, imgUrl) {
+    let id = this.id;
     let activity = new Activity(id, title, description, imgUrl);
     this.activities.push(activity);
+    this.id++;
   }
-  deleteActivity(id){
-    this.activities = this.activities.filter(element => element.id != id);
+  deleteActivity(id) {
+    this.activities = this.activities.filter((element) => element.id != id);
   }
 }
+
+let repository = new Repository();
+
+function convertirHtml(activity) {
+  let { id, title, description, imgUrl } = activity;
+  let h3 = document.createElement("h3");
+  let p = document.createElement("p");
+  let img = document.createElement("img");
+  h3.innerHTML = title;
+  p.innerHTML = description;
+  img.src = imgUrl;
+  h3.className = "sTitle";
+  p.className = "paragraph";
+  img.className = "foto";
+  let div = document.createElement("div");
+  div.append(h3, p, img);
+  div.className = "marco";
+  div.id = id;
+  return div;
+}
+
+function handlerDelete(id) {
+  let contenedor = document.getElementById("cards");
+  repository.deleteActivity(id);
+  let activities = repository.getAllActivities();
+  activities.length != 0
+    ? convertirActivities()
+    : (contenedor.innerHTML = "<p>Nada agregado aun.</p>");
+}
+
+function convertirActivities() {
+  let contenedor = document.getElementById("cards");
+  contenedor.innerHTML = "";
+  let activities = repository.getAllActivities();
+  let cards = activities.map(function (activity) {
+    return convertirHtml(activity);
+  });
+  cards.forEach((card) => {
+    contenedor.appendChild(card);
+    card.addEventListener("click", () => handlerDelete(card.id));
+  });
+}
+
+function handlerClick() {
+  let inputTitulo = document.getElementById("titulo").value;
+  let inputDescription = document.getElementById("description").value;
+  let inputImg = document.getElementById("url").value;
+  if (inputTitulo == "" || inputDescription == "" || inputImg == "") {
+    alert("Debe completar todos los campos");
+  } else {
+    repository.createActivity(inputTitulo, inputDescription, inputImg);
+    document.getElementById("titulo").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("url").value = "";
+    convertirActivities();
+  }
+}
+
+if (typeof window != "undefined") {
+  //Agregué esta condición para que Jasmine no me detecte un error de -document is undefined-
+  let button = document.getElementById("add");
+  button.addEventListener("click", () => handlerClick());
+}
+
+module.exports = {
+  Activity,
+  Repository,
+};
--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,5 +1,97 @@
-describe("demo", function () {
+/*describe("demo", function () {
   it("Este test debe pasar siempre", function () {
     expect(4 + 2).toBe(6);
+  });
+});*/
+
+const { Repository, Activity } = require("../scripts/index");
+describe("La clase Repository: ", () => {
+  it("Debe ser una clase", () => {
+    expect(typeof Repository.prototype.constructor).toBe("function");
+  });
+  it("Debe tener implementado el método getAllActivities()", () => {
+    let repository = new Repository();
+    expect(repository.getAllActivities).toBeDefined();
+  });
+  it("Debe tener implementado el método createActivity()", () => {
+    let repository = new Repository();
+    expect(repository.createActivity).toBeDefined();
+  });
+  it("Debe tener implementado el método deleteActivity()", () => {
+    let repository = new Repository();
+    expect(repository.deleteActivity).toBeDefined();
+  });
+  it("El método getAllActivities() debe retornar un array", () => {
+    let repository = new Repository();
+    expect(Array.isArray(repository.getAllActivities())).toBeTruthy();
+  });
+  it("El método createActivity() debe agregar un elemento", () => {
+    let repository = new Repository();
+    repository.createActivity(
+      "Perro",
+      "Guau",
+      "https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg"
+    );
+    expect(repository.getAllActivities()).toEqual([
+      new Activity(
+        0,
+        "Perro",
+        "Guau",
+        "https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg"
+      ),
+    ]);
+  });
+  it("El método deleteActivity() debe eliminar la ultima actividad", () => {
+    let repository = new Repository();
+    repository.createActivity(
+      "Perro",
+      "Guau",
+      "https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg"
+    );
+    repository.createActivity(
+      "Gato",
+      "Miau",
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FGato_atigrado&psig=AOvVaw1bB_agyE3rtVYIIiNwMA5D&ust=1726016195557000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDX78aVt4gDFQAAAAAdAAAAABAE"
+    );
+    repository.createActivity(
+      "Gallo",
+      "Cocoroco",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/640px-Male_and_female_chicken_sitting_together.jpg"
+    );
+    repository.deleteActivity(1);
+    expect(repository.getAllActivities()).toEqual([
+      new Activity(
+        0,
+        "Perro",
+        "Guau",
+        "https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg"
+      ),
+      new Activity(
+        2,
+        "Gallo",
+        "Cocoroco",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/640px-Male_and_female_chicken_sitting_together.jpg"
+      ),
+    ]);
+    expect(repository.getAllActivities()).not.toEqual([
+      new Activity(
+        0,
+        "Perro",
+        "Guau",
+        "https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg"
+      ),
+      new Activity(
+        1,
+        "Gato",
+        "Miau",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FGato_atigrado&psig=AOvVaw1bB_agyE3rtVYIIiNwMA5D&ust=1726016195557000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDX78aVt4gDFQAAAAAdAAAAABAE"
+      ),
+      new Activity(
+        2,
+        "Gallo",
+        "Cocoroco",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/640px-Male_and_female_chicken_sitting_together.jpg"
+      ),
+    ]);
   });
 });
